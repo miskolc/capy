478bdbfc7fad382825d76902be8131b21ae48578
Dragos Calin <calinho_gx1992@yahoo.com>
2014-05-16 23:07:52 +0300

    Modificari Capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{ubuntu@54.187.190.252}
+role :web, %w{ubuntu@54.187.190.252}
+role :db,  %w{ubuntu@54.187.190.252}
 
 # Extended Server Syntax
 # ======================
@@ -14,8 +14,10 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :stage, :production
 
+# Replace 127.0.0.1 with your server's IP address!
+server '54.187.190.252', user: 'ubuntu', roles: %w{web app}
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
